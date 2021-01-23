7b69de1757290776d4ea8fc2368845cd6cbfb92c
NAZARIILUNYCH <47526149+NAZARIILUNYCH@users.noreply.github.com>
2021-01-23 18:21:32 +0200

    todoList

---
+++ lesson6/task3/src/list/taskGateway.js
@@ -0,0 +1,31 @@
+const baseUrl = 'https://5fedddb29708250017ce3f18.mockapi.io/api/v1/tasks';
+
+const mapTasks = tasks => tasks.map(({ id, ...rest }) => ({ ...rest, id }));
+
+export const getTasksList = () =>
+  fetch(baseUrl)
+    .then(response => response.json())
+    .then(tasks => mapTasks(tasks));
+
+export const createTask = taskData =>
+  fetch(baseUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8',
+    },
+    body: JSON.stringify(taskData),
+  });
+
+export const updateTask = (taskId, updatedTaskData) =>
+  fetch(`${baseUrl}/${taskId}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8',
+    },
+    body: JSON.stringify(updatedTaskData),
+  });
+
+export const deleteTask = taskId =>
+  fetch(`${baseUrl}/${taskId}`, {
+    method: 'DELETE',
+  });
