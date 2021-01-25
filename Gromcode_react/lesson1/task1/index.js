299f35c00634c4db15b7c2db15c54a27569a0535
NAZARIILUNYCH <47526149+NAZARIILUNYCH@users.noreply.github.com>
2021-01-25 15:39:07 +0200

    first App

---
+++ Gromcode_react/lesson1/task1/index.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-undef */
+const rootElement = document.querySelector('#root');
+
+// const greetingElement = document.createElement('div');
+// greetingElement.classList.add('greeting');
+// greetingElement.textContent = 'Hello, React!';
+
+// rootElement.append(greetingElement);
+
+const greetingElement = React.createElement('div', { className: 'greeting' }, 'Hello, React');
+
+ReactDOM.render(greetingElement, rootElement);
